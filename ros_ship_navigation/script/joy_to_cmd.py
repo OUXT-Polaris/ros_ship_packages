a46f6b07188ae9619e2146559034d1bf8b2dbeee
Masaya Kataoka <masaya.kataoka@ams.eng.osaka-u.ac.jp>
2017-09-16 22:04:27 +0900

    update parameters

--- ros_ship_navigation/script/joy_to_cmd.py
+++ ros_ship_navigation/script/joy_to_cmd.py
@@ -5,14 +5,15 @@ from std_msgs.msg import Float64
 
 class joy_controlelr:
     def __init__(self):
+        self.max_value = rospy.get_param(rospy.get_name()+"/max_value")
         self.left_motor_cmd_pub = rospy.Publisher("/wam_v/left_motor_controller/command",Float64,queue_size=1)
         self.right_motor_cmd_pub = rospy.Publisher("/wam_v/right_motor_controller/command",Float64,queue_size=1)
         self.joy_sub = rospy.Subscriber("/joy", Joy, self.joy_callback,queue_size=1)
     def joy_callback(self,data):
         self.left_motor_cmd = Float64()
         self.right_motor_cmd = Float64()
-        self.left_motor_cmd.data = data.axes[1]
-        self.right_motor_cmd.data = data.axes[4]
+        self.left_motor_cmd.data = data.axes[1]*self.max_value
+        self.right_motor_cmd.data = data.axes[4]*self.max_value
         self.left_motor_cmd_pub.publish(self.left_motor_cmd)
         self.right_motor_cmd_pub.publish(self.right_motor_cmd)
 
