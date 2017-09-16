c1ffead160bed3ba5744824f87bdbf0d0d0d3e84
Masaya Kataoka <masaya.kataoka@ams.eng.osaka-u.ac.jp>
2017-09-16 23:34:50 +0900

    add twist_from_odom node

---
+++ ros_ship_navigation/script/twist_from_odom.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+
+class twist_calculator:
+    def __init__(self):
+        self.twist_pub = rospy.Publisher("/robot_velocity", Twist, queue_size=1)
+        self.pose_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback,queue_size=1)
+        self.publish_enable = False
+        self.last_elapsed_time = rospy.Time.now()
+        self.now = rospy.Time.now()
+        self.twist_msg = Twist()
+    def odom_callback(self,data):
+        self.twist_msg = data.twist.twist
+        self.twist_pub.publish(self.twist_msg)
+
+if __name__ == '__main__':
+    rospy.init_node('twist_from_odom_node.py', anonymous=True)
+    calculator = twist_calculator()
+    rospy.spin()
