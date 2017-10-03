231e9aebb959469b89ddc6a094f5985f5b32663b
Masaya Kataoka <masaya.kataoka@ams.eng.osaka-u.ac.jp>
2017-10-04 01:17:11 +0900

    add driving force topic

--- ros_ship_control/include/driving_force_controller.h
+++ ros_ship_control/include/driving_force_controller.h
@@ -8,6 +8,19 @@
 //headers for fardware_interface
 #include <hardware_interface/joint_command_interface.h>
 
+//headers for realtime_tools
+#include <realtime_tools/realtime_publisher.h>
+#include <realtime_tools/realtime_buffer.h>
+
+//headers for ROS
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/Float32.h>
+
+//headers for boost
+#include <boost/shared_ptr.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/thread/condition.hpp>
+
 namespace driving_force_controller
 {
   class DrivingForceController: public controller_interface::Controller<hardware_interface::VelocityJointInterface>
@@ -16,10 +29,42 @@ namespace driving_force_controller
     DrivingForceController();
     ~DrivingForceController();
   private:
-    virtual bool init(hardware_interface::VelocityJointInterface* hw,ros::NodeHandle& controller_nh);
-    virtual void starting(const ros::Time& time);
-    virtual void update(const ros::Time& time, const ros::Duration& period);
-    virtual void stopping(const ros::Time& time);
+    void twistCallback(const geometry_msgs::Twist& msg);
+    void drivingForceCallback(const std_msgs::Float32& msg);
+    bool init(hardware_interface::VelocityJointInterface* hw,ros::NodeHandle& controller_nh);
+    void starting(const ros::Time& time);
+    void update(const ros::Time& time, const ros::Duration& period);
+    void stopping(const ros::Time& time);
+
+    boost::scoped_ptr<realtime_tools::RealtimePublisher<std_msgs::Float32> > motor_command_publisher;
+
+    struct Twist
+    {
+      double lin_x;
+      double lin_y;
+      double lin_z;
+      double ang_x;
+      double ang_y;
+      double ang_z;
+      ros::Time stamp;
+      Twist() : lin_x(0.0), lin_y(0.0), lin_z(0.0), ang_x(0.0), ang_y(0.0), ang_z(0.0), stamp(0.0) {}
+    };
+    realtime_tools::RealtimeBuffer<Twist> twist;
+    Twist twist_struct;
+    ros::Subscriber sub_twist;
+
+    struct DrivingForce
+    {
+      double value;
+      ros::Time stamp;
+      DrivingForce() : value(0.0) {}
+    };
+    realtime_tools::RealtimeBuffer<DrivingForce> driving_force;
+    DrivingForce driving_force_struct;
+    ros::Subscriber driving_force_sub;
+
+    //parameters
+    std::string twist_topic,motor_command_topic,driving_force_command_topic;
   };
 }
 #endif
--- ros_ship_control/src/driving_force_controller.cpp
+++ ros_ship_control/src/driving_force_controller.cpp
@@ -7,6 +7,15 @@
 //headers for pluginlib
 #include <pluginlib/class_list_macros.h>
 
+//headers for realtime_tools
+#include <realtime_tools/realtime_publisher.h>
+#include <realtime_tools/realtime_buffer.h>
+
+//headers for boost
+#include <boost/shared_ptr.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/thread/condition.hpp>
+
 namespace driving_force_controller
 {
   DrivingForceController::DrivingForceController()
@@ -19,17 +28,59 @@ namespace driving_force_controller
 
   }
 
+  void DrivingForceController::drivingForceCallback(const std_msgs::Float32& msg)
+  {
+    if (isRunning())
+    {
+      driving_force_struct.value = msg.data;
+      driving_force.writeFromNonRT(driving_force_struct);
+    }
+    else
+    {
+      ROS_ERROR("Can't accept new commands. Controller is not running.");
+    }
+  }
+
+  void DrivingForceController::twistCallback(const geometry_msgs::Twist& msg)
+  {
+    if (isRunning())
+    {
+      twist_struct.ang_x = msg.angular.x;
+      twist_struct.ang_y = msg.angular.y;
+      twist_struct.ang_z = msg.angular.z;
+      twist_struct.lin_x = msg.linear.x;
+      twist_struct.lin_y = msg.linear.y;
+      twist_struct.lin_z = msg.linear.z;
+      twist_struct.stamp = ros::Time::now();
+      twist.writeFromNonRT(twist_struct);
+    }
+    else
+    {
+      ROS_ERROR("Can't accept new commands. Controller is not running.");
+    }
+  }
+
   bool DrivingForceController::init(hardware_interface::VelocityJointInterface* hw,ros::NodeHandle& controller_nh)
   {
-
+    controller_nh.getParam("twist_topic",twist_topic);
+    controller_nh.getParam("motor_command_topic",motor_command_topic);
+    controller_nh.getParam("driving_force_command_topic",driving_force_command_topic);
+    motor_command_publisher.reset(new realtime_tools::RealtimePublisher<std_msgs::Float32>(controller_nh, motor_command_topic, 1));
+    sub_twist = controller_nh.subscribe(twist_topic, 1, &DrivingForceController::twistCallback, this);
+    driving_force_sub = controller_nh.subscribe(driving_force_command_topic, 1, &DrivingForceController::drivingForceCallback, this);
   }
+
   void DrivingForceController::starting(const ros::Time& time)
   {
 
   }
+
   void DrivingForceController::update(const ros::Time& time, const ros::Duration& period)
   {
+    if(motor_command_publisher && motor_command_publisher->trylock())
+    {
 
+    }
   }
   void DrivingForceController::stopping(const ros::Time& time)
   {
